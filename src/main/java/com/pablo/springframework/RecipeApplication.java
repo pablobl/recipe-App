1cd2fa7eade1fcd3498681ab7ec95340c54903d7
Pablo Blazquez <pbl@foryouandyourcustomers.com>
2019-01-17 11:28:35 +0100

    Completing assignment to create 1-1 relation between Ingredient and UnitOfMeasure.

--- src/main/java/com/pablo/springframework/RecipeApplication.java
+++ src/main/java/com/pablo/springframework/RecipeApplication.java
@@ -1,4 +1,4 @@
-package com.pablo.recipe;
+package com.pablo.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/pablo/springframework/controllers/IndexController.java
+++ src/main/java/com/pablo/springframework/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.pablo.recipe.controllers;
+package com.pablo.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
